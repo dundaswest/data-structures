b05409f244bc33361ee487fc574b8ccea2d90d70
Eunbyul Cho <hello@eunbyulcho.me>
2018-04-25 21:04:29 -0700

    complete stack using es6

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,22 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.mysize = 0;
   }
+
+  push (value) {
+    this.mysize++;
+    this[this.mysize] = value;
+  }
+
+  pop () {
+    this.mysize--;
+    return this[this.mysize+1];
+  }
+
+  size () {
+    return Math.max(this.mysize,0);
+  }
+
 
 }
