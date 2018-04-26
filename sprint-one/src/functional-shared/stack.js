3f019883b831776989be932ea8a792145b689a1f
Eunbyul Cho <hello@eunbyulcho.me>
2018-04-25 18:34:32 -0700

    use functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.mysize = 0;
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+
+
+var stackMethods = {
+  push: function(value) {
+    this.mysize++;
+    this[this.mysize] = value;
+  },
+  pop : function() {
+    this.mysize--;
+    return this[this.mysize+1];
+  },
+
+  size : function() {
+    return Math.max(this.mysize,0);
+  }
+};
 
 
