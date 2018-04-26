2aac22f3ac82ae04f2d86ca887bb79cbea511b63
Eunbyul Cho <hello@eunbyulcho.me>
2018-04-25 20:40:44 -0700

    make stack work using pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.mysize = 0;
 };
+
+Stack.prototype.push = function(value) {
+  this.mysize++;
+  this[this.mysize] = value;
+};
+
+Stack.prototype.pop = function() {
+  this.mysize--;
+  return this[this.mysize+1];
+};
+
+Stack.prototype.size = function() {
+  return Math.max(this.mysize,0);
+};
+
+var myStack = new Stack();
 
 
