3b8d8ed6f46acce6aaa52b756c85d6bdd5d32ef8
dundaswest <hello@eunbyulcho.me>
2018-04-26 21:24:56 -0700

    passed all tests for Tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  
 
   return newTree;
 };
@@ -11,13 +11,32 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var childTree = Tree(value);
+  this.children.push(childTree);
+  console.log(this.children)
 };
 
 treeMethods.contains = function(target) {
+  
+  var checkTree = function(node) {
+    if(node.value === target) {
+      return true;
+    }
+    
+    for(var i = 0; i < node.children.length; i++){
+      if (checkTree(node.children[i])){
+        return true;
+      }
+    }
+      
+    return false;     
+  }
+  return checkTree(this);
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Answer: linear time complexity
  */
