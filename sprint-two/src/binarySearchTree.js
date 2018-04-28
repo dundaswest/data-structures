7e411f2505ed91798431c7b362e038a4f8978372
dundaswest <hello@eunbyulcho.me>
2018-04-27 22:32:15 -0700

    added some skeleton methods to binarysearchtree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,31 @@
 var BinarySearchTree = function(value) {
+
+  var newTree = {};
+  
+  newTree.value = value;
+  newTree.left;
+  newTree.right;
+  
+  _.extend(newTree, treeMethods);
+
+  newTree.children = [];  
+
+  return newTree;
+
 };
 
+var treeMethods = {};
+
+treeMethods.insert = function(value){
+  var treeNode = BinarySearchTree(value);
+  this.children.push(treeNode);
+  //
+  
+};
+treeMethods.contains = function(){};
+treeMethods.depthFirstLog = function(){};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ /*https://www.geeksforgeeks.org/implementation-binary-search-tree-javascript/*/
